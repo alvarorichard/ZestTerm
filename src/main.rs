c0cf87353b25beb0ef2fcd5e784ee32ad838b2d2
alvarorichard <alvaro10048@protonmail.com>
2025-02-09 23:44:45 -0300

    Accept Multiple Commands

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use std::{io::stdin, process::Command};
+use std::io::{stdout, Write};
 
 fn main() {
+    loop {
+        print!(">");
+        stdout().flush();
+
+    
  let mut input = String::new();
  stdin().read_line(&mut input).unwrap();
 
  let command = input.trim();
 
-    Command::new(command)
+   let mut child = Command::new(command)
     .spawn()
     .unwrap();
+    
+    child.wait();
+    }
 
 }
