d5ed8fd7e6b53bfe46cb4328bfc235735366d643
alvarorichard <alvaro10048@protonmail.com>
2025-02-09 23:56:08 -0300

    Shell Built-ins

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,32 @@
-use std::{io::stdin, process::Command};
 use std::io::{stdout, Write};
+use std::path::Path;
+use std::{io::stdin, process::Command};
 
 fn main() {
     loop {
         print!(">");
         stdout().flush();
 
-    
- let mut input = String::new();
- stdin().read_line(&mut input).unwrap();
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
 
- let mut parts = input.trim().split_whitespace();
- let command = parts.next().unwrap();
- let args = parts;
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
 
-   let mut child = Command::new(command)
-    .args(args)
-    .spawn()
-    .unwrap();
-    
-    child.wait();
+        match command {
+            "cd" => {
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = std::env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                }
+            }
+            command => {
+                let mut child = Command::new(command).args(args).spawn().unwrap();
+
+                child.wait();
+            }
+        }
     }
-
 }
